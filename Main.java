41e1095e3c3ea6fcce11d7a98b7faffdca400ca7
Felipe <felipemoreira1@gmail.com>
2022-04-18 20:57:03 -0300

    Stage 4/8: Add subtractions
    Usei regex para reconhecer os padrões mas ainda estou achando a solução
    feia pra c%r%#$o. É bem possível que eu precise refazê-la na próxima
    etapa.

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
-        
+
         while (!exit) {
             String line = scanner.nextLine();
             if (!"".equals(line)) {
@@ -16,16 +15,46 @@ public class Main {
                 } else if ("/help".equals(line)) {
                     System.out.println("The program calculates the sum of numbers");
                 } else {
-                    
-                    String[] inputs = line.split("\\s+");
-                    int sum = Arrays.stream(inputs).mapToInt(Integer::parseInt).sum();
-                    System.out.println(sum);
-                                        
+
+                    String[] expression = line.split("\\s+");
+                    int result = calculate(expression);
+                    System.out.println(result);
+
                 }
-            } 
-            
+            }
+
         }
         scanner.close();
-        
+
+    }
+
+    public static int calculate(String[] expression) {
+        if (expression.length == 1) {
+            return Integer.parseInt(expression[0]);
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < expression.length; i++) {
+            if (expression[i].matches("\\d+")) {
+                sum += Integer.parseInt(expression[i]);
+            } else if (expression[i].matches("[-]\\d")) {
+                sum += -1 * Integer.parseInt(expression[i].substring(1));
+            } else if (expression[i].matches("[+]\\d")) {
+                sum += Integer.parseInt(expression[i].substring(1));
+            } else if (expression[i].matches("[+]+")){
+                sum += Integer.parseInt(expression[i + 1]);
+                i++;
+            } else if (expression[i].matches("[-]+")){
+                if (expression[i].length() % 2 == 0) {
+                    sum += Integer.parseInt(expression[i + 1]);
+                } else {
+                    sum += -1 * Integer.parseInt(expression[i + 1]);
+                }
+                i++;
+            }
+        }
+
+        return sum;
     }
 }
